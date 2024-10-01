22db4c609be1da6f9bdbd80f8566d0eac721efac
nicoleads <nicole.d.moreira@aluno.senai.br>
2024-10-01 10:30:56 -0300

    Bet Fortalece
    jogo de azar

---
+++ jogo.php
@@ -0,0 +1,9 @@
+<?php
+$numero = $_POST['numero'];
+$numero_sorte = $numero+1;
+
+echo"numero informado: ", $numero;
+echo"<br>";
+echo"Ops! Quase acertou";
+echo"<br>";
+echo"Numero sorteado: ", $numero_sorte;
